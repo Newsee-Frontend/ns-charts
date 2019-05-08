a8024d22bdb9975593c601d160ca19e9f48b715a
BroccoliSpring <Lensgcx@163.com>
2019-05-08 15:21:28 +0800

    Update babel.config.js

--- babel.config.js
+++ babel.config.js
@@ -9,16 +9,17 @@ module.exports = {
                 "targets": {
                     "browsers": [
                         "> 1%",
-                        "last 2 versions",
-                        "not ie <= 6"
+                        "last 5 versions",
+                      "ie >= 8"
                     ]
                 }
             }
-        ]
+        ],
     ],
     "plugins": [
         "@babel/plugin-transform-runtime",
         "@babel/plugin-syntax-dynamic-import",
+        "@babel/plugin-transform-object-assign",
         ["@babel/plugin-proposal-decorators", {"legacy": true}],
         ["@babel/plugin-proposal-class-properties", {"loose": true}],
         "@babel/plugin-syntax-jsx",
