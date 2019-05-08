23c7e8d3635aaf925cdfc32b51bab779d1edde84
xilanhuadechuntian <Lensgcx@163.com>
2019-05-08 15:49:30 +0800

    Update main.js

--- docs/SPA/src/main.js
+++ docs/SPA/src/main.js
@@ -1,5 +1,6 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+import '@babel/polyfill'
 import Vue from 'vue';
 import App from './App';
 
@@ -9,6 +10,6 @@ import './assets/index.scss'
 /* eslint-disable no-new */
 new Vue({
     el: '#app',
-    components: { App },
+    components: {App},
     template: '<App/>',
 });
