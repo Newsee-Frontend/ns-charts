705114e94f28da203e33031fe39d82a29d5144e7
luoyi <2322113782@qq.com>
2019-08-22 09:59:49 +0800

    一些改动

--- src/config/colorList.js
+++ src/config/colorList.js
@@ -4,6 +4,12 @@ import echarts from 'echarts';
 export default [
     {
         normal: {
+            label:{
+                show: false,
+                position: 'top',
+                distance: 0,
+                color: '#333'
+            },
             color: new echarts.graphic.LinearGradient(0, 0, 0, 1,
                 [
                     //颜色渐变函数 前四个参数分别表示四个位置依次为左、下、右、上
@@ -14,6 +20,12 @@ export default [
     },
     {
         normal: {
+            label:{
+                show: false,
+                position: 'top',
+                distance: 0,
+                color: '#333'
+            },
             color: new echarts.graphic.LinearGradient(0, 0, 0, 1, [
                 {offset: 0, color: '#ffa87a'},
                 {offset: 1, color: '#ff6161'}
@@ -22,6 +34,12 @@ export default [
     },
     {
         normal: {
+            label:{
+                show: false,
+                position: 'top',
+                distance: 0,
+                color: '#333'
+            },
             color: new echarts.graphic.LinearGradient(0, 0, 0, 1, [
                 {offset: 0, color: '#a5ec27'},
                 {offset: 1, color: '#4ed552'}
@@ -30,6 +48,12 @@ export default [
     },
     {
         normal: {
+            label:{
+                show: false,
+                position: 'top',
+                distance: 0,
+                color: '#333'
+            },
             color: new echarts.graphic.LinearGradient(0, 0, 0, 1, [
                 {offset: 0, color: '#11d6cb'},
                 {offset: 1, color: '#23c8c4'}
